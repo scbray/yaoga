4024fa5df54cbf12619d2678fa3c64952c845a84
Steve <na>
2011-11-22 22:45:02 -0600

    adapted the new user blurp
    This had been customized live on the website.
    Now merging it to version control, but after the recent changes.

--- wp-content/themes/nycga/header.php
+++ wp-content/themes/nycga/header.php
@@ -37,29 +37,19 @@
 		<?php do_action( 'bp_after_header' ) ?>
 		<?php do_action( 'bp_before_container' ) ?>
 		
-		<div id="blurb" style="height:170px">
-               <div style="float:left;">
-                       <iframe width="250" height="170" src="http://www.youtube.com/embed/Sj66DB_PKz0" frameborder="0" allowfullscreen></iframe>
-               </div>
-               <div style="margin-left:260px">
-                       <h4 style="margin-bottom:6px">Thursday, November 17, 2011</h4>
-                       <h3 style="margin-bottom:6px">International Day of Action</h3>
-                       <h4 style="margin-bottom:6px;">Find Out More: <a href="http://occupywallst.org/action/november-17th/" target="_blank">http://occupywallst.org/action/november-17th/</a></h4>
-                       <h4>Get Housing Information: <a href="http://www.nycga.net/resources/housing-support/" target="_blank">http://www.nycga.net/resources/housing-support/</a></h4>
-               </div>
-       </div>
 		<? if( ! is_user_logged_in() ){ ?>
-			<!-- <div style="float:left;">
+		<div id="blurb" style="height:170px;">
+			<div style="float:left;">
 				<iframe width="335" height="170" src="https://www.youtube.com/embed/6dtD8RnGaRQ?rel=0" frameborder="0" allowfullscreen></iframe>
 			</div>
 			<div style="margin-left:355px">
-				<h3 style="margin-bottom:6px;">Welcome to the new <em>and improved</em> New York City General Assembly<br/>Currently occupying Zuccotti Park.</h3>
-				Read our <a href="/resources/declaration/">Declaration</a><br />
+				<h3 style="margin-bottom:6px;">Welcome to the official website of the Peoria Illinois General Assembly</h3>
+				Read the New York City General Assembly <a href="http://www.nycga.net/resources/declaration/">Declaration</a><br />
 				<a href="/register">Create an Account</a><br />
 				Join some <a href="/groups">groups</a><br />
-				or <a href="/how-to-help/">find other ways to help</a>
+				or find other ways to help
 			</div>
-		</div> -->
+		</div>
 		<? } ?>
 
 		<div id="container">
